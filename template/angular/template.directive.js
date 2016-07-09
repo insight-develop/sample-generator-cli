ac50af8c1337bec2f4b8e89d8960e339f18e284f
Rodrigo Edamatsu <Rodrigo Edamatsu>
2016-07-09 12:26:29 -0300

    create angular templates

---
+++ template/angular/template.directive.js
@@ -0,0 +1,13 @@
+/**
+ * Just a directive example
+ * */
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('_DIRECTIVE_TEMPLATE_', _DIRECTIVE_TEMPLATE_);
+
+    function _DIRECTIVE_TEMPLATE_() {
+    }
+})();
